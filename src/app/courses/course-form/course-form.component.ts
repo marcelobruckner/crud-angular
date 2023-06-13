2e7ee50b91f137fbc867651c38ab743c9fc08a1f
Marcelo Bruckner <marcelobruckner@gmail.com>
2023-06-13 13:43:05 -0300

    Implementando a criação de cursos.

--- src/app/courses/course-form/course-form.component.ts
+++ src/app/courses/course-form/course-form.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { CoursesService } from '../services/courses.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { config } from 'rxjs';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-course-form',
@@ -10,7 +14,10 @@ export class CourseFormComponent implements OnInit {
 
   form: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) {
+  constructor(private formBuilder: FormBuilder,
+    private service: CoursesService,
+    private snackBar: MatSnackBar,
+    private location: Location) {
     this.form = this.formBuilder.group({
       name: [null],
       category: [null]
@@ -21,10 +28,22 @@ export class CourseFormComponent implements OnInit {
   }
 
   onSubmit(){
-    console.log('onSubmit');
+    this.service.save(this.form.value)
+    .subscribe(
+      result => this.onSuccess(),
+      error => this.onError());
   }
 
   onCancel(){
-    console.log('onCancel');
+    this.location.back();
+  }
+
+  onSuccess(){
+    this.snackBar.open('Curso salvo com sucesso!', '', { duration: 5000 });
+    this.onCancel();
+  }
+
+  onError(){
+    this.snackBar.open('Erro ao salvar novo curso', 'Fechar', { duration: 5000 });
   }
 }
